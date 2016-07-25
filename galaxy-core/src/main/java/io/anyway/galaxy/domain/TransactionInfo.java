e2c3e91aff62d77d123c29259f1d3ee052697787
jinfei21 <63581560@qq.com>
2016-07-25 18:58:01 +0800

    no message

--- galaxy-core/src/main/java/io/anyway/galaxy/domain/TransactionInfo.java
+++ galaxy-core/src/main/java/io/anyway/galaxy/domain/TransactionInfo.java
@@ -42,11 +42,11 @@ public class TransactionInfo {
 
     
     public String getStrGmtCreated(){
-    	return new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(gmtCreated);
+    	return sdf.format(gmtCreated);
     }
     
     public String getStrGmtModified(){
-    	return new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(gmtModified);
+    	return sdf.format(gmtModified);
     }
     
     public String getStrTXType(){
